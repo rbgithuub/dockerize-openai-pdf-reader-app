3447f5a66a720f5d305e048a17bd7d2a3c4d5acf
Balaji Raja <net.balajiraja@gmail.com>
2024-07-12 02:12:39 +0300

    Initial docker config of openai pdf reader app

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+﻿FROM python:3.12
+WORKDIR /app
+COPY . /app
+#RUN pip install openai
+RUN pip install --no-cache-dir --requirement  /app/requirements.txt
+EXPOSE 8080
+CMD [“python”, “./openai-internal-kb.py”] 
---
+++ openai-internal-kb.py
@@ -0,0 +1,52 @@
+﻿#Load OpenAI API Key
+#os.environ["OPENAPI_API_KEY"] = openai.api_key
+from openai import OpenAI
+#from openai.embeddings_utils import get_embedding
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+#from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+
+#Setup Loader - in this case a PDF Loader
+loader = PyMuPDFLoader("suse_administration.pdf")
+
+#Load and split the pdf into pages
+pages = loader.load_and_split()
+
+# setup a text splitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100,
+    length_function=len,
+)
+
+#split pages into smaller chunks called docs
+docs = text_splitter.split_documents(pages)
+
+#transform to embeddings
+embeddings = OpenAIEmbeddings()
+
+#setup and store docs and embeddings into ChromaDB
+vectordb = Chroma.from_documents(docs, embedding=embeddings,
+                                 persist_directory=".")
+
+#Make the database persisten
+vectordb.persist()
+
+#setup memory so it remembers previous questions and answers
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+#Perform the conversational Retrieval Chain
+qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.5),vectordb.as_retriever(), memory=memory)
+
+#Run the question
+question = "Explain grouping and combining commands in Suse linux.?"
+result = qa.run(question)
+
+#print the values to the screen
+print(result)
